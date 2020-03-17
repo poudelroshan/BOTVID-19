ca5f022458dafd5407134bf9ba3ea77af6dd1c2b
poudelroshan <ro0shan29@gmail.com>
2020-03-18 01:25:35 +0900

    Added python scripts for the bot

---
+++ scrape.py
@@ -0,0 +1,35 @@
+#from time import gmtime, strftime
+import datetime
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+URL = "https://coronamap.site/"
+DATABASE = ["time&date", 0, 0, 0] #[time, confirmed, death, recovered]
+
+def output(confirmed, recovered, death, time):
+    return "Welcome to Corona Bot by ROSHAN POUDEL\n" + "Current Local time: " + time + "\n" + "Confirmed Cases: " + confirmed.text + "\n" + "Deaths: " + death.text + "\n" + "Recovered: " + recovered.text
+
+def corona():
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
+    driver.get(URL)
+    english_lang = driver.find_element_by_class_name("english-btn")
+    english_lang.click()
+    close_button = driver.find_element_by_class_name("notice-btn")
+    close_button.click()
+    
+    confirmed = driver.find_element_by_xpath("/html/body/div[8]/div[1]/div")
+    recovered = driver.find_element_by_xpath("/html/body/div[8]/div[2]/div[1]/div[2]")
+    death = driver.find_element_by_xpath("/html/body/div[8]/div[2]/div[3]/div[2]")
+    time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M KST")
+
+    value = output(confirmed, recovered, death, time)
+    driver.quit()
+    return value
+
+if __name__ == "__main__":
+    print(corona())
+
+    
