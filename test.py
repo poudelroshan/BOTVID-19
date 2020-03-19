d6a6b156f3d9b9d21ecd960c29c6de28f7019579
poudelroshan <ro0shan29@gmail.com>
2020-03-19 15:10:51 +0900

    changed filename

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ import random
 from pymessenger.bot import Bot
 import scrape
 
-app = Flask(__name__)
+test = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     return "<h1> This test page works! </h1>"
 
 if __name__ == "__main__":
-    app.run(threaded=True)
+    test.run(threaded=True)
