cdb077a8548ed428e4b1610925cd03027488660c
poudelroshan <ro0shan29@gmail.com>
2020-03-27 00:13:46 +0900

    made changes to data_creator.py

--- data_creator.py
+++ data_creator.py
@@ -1,20 +1,67 @@
 import sqlite3
 
-connection = sqlite3.connect("TEST.db")
+connection = sqlite3.connect("numbers.db")
 cursor = connection.cursor()
 
-'''cursor.execute("""CREATE TABLE users(
-user_id integer,
-user_role text
-)""")'''
+cursor.execute("""CREATE TABLE corona(
+state text,
+cases integer,
+deaths integer
+)""")
 
 with connection:
-    cursor.execute("INSERT INTO test VALUES (3523, 'user')")
-    cursor.execute("INSERT INTO test VALUES (3523, 'user')")
-    cursor.execute("INSERT INTO test VALUES (3523, 'user')")
-    cursor.execute("INSERT INTO test VALUES (3523, 'user')")
-    cursor.execute("INSERT INTO test VALUES (3523, 'user')")
-    cursor.execute("INSERT INTO test VALUES (3523, 'user')")
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AK', 3200, 222)") #Alaska
+    cursor.execute("INSERT INTO corona VALUES ('AZ', 3200, 222)") #Arizona
+    cursor.execute("INSERT INTO corona VALUES ('AR', 3200, 222)") #Arkansas
+    cursor.execute("INSERT INTO corona VALUES ('CA', 3200, 222)") #California
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+        cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+        cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+        cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+    cursor.execute("INSERT INTO corona VALUES ('AL', 3200, 222)") #Alabama
+
+
+
+
+
+
+
+
+    
+    cursor.execute("INSERT INTO test VALUES ('New York', 3200, 222)")
+    cursor.execute("INSERT INTO test VALUES ('New York', 3200, 222)")
+    cursor.execute("INSERT INTO test VALUES ('New York', 3200, 222)")
+    cursor.execute("INSERT INTO test VALUES ('New York', 3200, 222)")
+    cursor.execute("INSERT INTO test VALUES ('New York', 3200, 222)")
+    cursor.execute("INSERT INTO test VALUES ('New York', 3200, 222)")
+
 
 
 
