719efff49a2e0fe469b84f22afbeabcdc4f5decf
poudelroshan <poudelroshan@github.com>
2020-04-26 20:27:11 +0900

    using mysql.connector instead of pymysql for connection with MYSQL database

--- user_database.py
+++ user_database.py
@@ -1,8 +1,5 @@
 import authenticate
 
-connection = authenticate.connection
-cursor = connection.cursor()
-
 '''
 #ONLY USE FOR THE FIRST TIME TO CREATE THE TABLE
 cursor.execute("""CREATE TABLE users(user_id int)""")
@@ -10,34 +7,53 @@ connection.commit()
 '''
 
 def add_user(user_id):
-    connection.ping(reconnect=True)
-    with connection:
-        cursor.execute("INSERT INTO users VALUES (%s)", (user_id,))
-        connection.commit()
-    print("User Added to Database")
+    connection = authenticate.connection
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO users VALUES (%s)", (user_id,))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    print("User ", user_id, " Added to Database")
 
 def remove_user(user__id):
-    connection.ping(reconnect=True)
-    with connection:
-        cursor.execute("DELETE FROM users WHERE user_id = %s", (user__id,))
-        connection.commit()
-    print("User Removed from Database")
+    connection = authenticate.connection
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM users WHERE user_id = %s", (user__id,))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    print("User ", user__id, " Removed from Database")
 
     
 def is_user_subscribed(user__id):
+    connection = authenticate.connection
+    cursor = connection.cursor()
     cursor.execute("SELECT * FROM users WHERE user_id= %s", (user__id,))
-    return len(cursor.fetchall()) != 0
+    users = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return len(users) == 1
 
 def get_total_users():
+    connection = authenticate.connection
+    cursor = connection.cursor()
     cursor.execute("SELECT * FROM users")
-    return len(cursor.fetchall())
+    users = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return len(users)
     
     
 def get_users():
-    connection.ping(reconnect=True)
-    with connection:
-        cursor.execute("SELECT user_id FROM users")
-        return [x[0] for x in cursor.fetchall()]
+    connection = authenticate.connection
+    cursor = connection.cursor()
+    cursor.execute("SELECT user_id FROM users")
+    user_ids = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return [x[0] for x in user_ids]
+    
+
 
 
 
