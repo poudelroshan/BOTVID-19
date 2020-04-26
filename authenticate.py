8be7d4d7fe44c90802b0c2ecce9c81c870633a77
poudelroshan <poudelroshan@github.com>
2020-04-26 20:36:18 +0900

    fixing MYSQL no connection error

--- authenticate.py
+++ authenticate.py
@@ -11,7 +11,7 @@ connection = mysql.connector.connect(user='b1989099bde515',
                                      password='1064cb60',
                                      host='us-cdbr-iron-east-01.cleardb.net',
                                      db='heroku_28545fb12532e5e')
-
+connection.ping(reconnect=True)
 
 
 def verify_bot_access():
