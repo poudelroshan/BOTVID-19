b8f822a5bfca17c1d6b8b6f6d50fec4e085da63e
poudelroshan <poudelroshan@github.com>
2020-04-28 05:40:31 +0900

    set script.py to send automated updates every even hours at x:42

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-import scrape, botvid, user_database
+import scrape, botvid, user_database, authenticate
 from datetime import datetime
 
 Alert1 = "You received the message above because you have subscribed for periodic notifications from me. The subscription lasts for 24 hours before Facebook unsubscribes you automatically."
@@ -7,13 +7,16 @@ Alert2 = "If you want to keep receiving messages from me, you will need to send 
 def send_message():
     now = datetime.now()
     counter_mins = now.minute
-    if (counter_mins == 59):
+    counter_hours = now.hour
+    if (counter_hours % 2 == 0 and counter_mins == 42):
         for users in user_database.get_users():
             botvid.send_message(users, botvid.get_message())
             botvid.send_message(users, Alert1)
             botvid.send_message(users, Alert2)
-        
-    print("Sent automated update message to all users")
+            print("Sent automated update message to user: ", users)
+    
+    print("Data base updated at: ", now)
+    
 
 if __name__ == '__main__':
     scrape.corona() #update database
